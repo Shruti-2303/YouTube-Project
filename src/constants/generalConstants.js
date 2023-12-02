b0700cf93319d69fc0b5a1c6ab4043f20e7f344d
Shruti-2303 <sharmas23032001@gmail.com>
2023-12-03 02:31:34 +0530

    Base Commit

--- src/constants/generalConstants.js
+++ src/constants/generalConstants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyA5_uf6bx92viTbscSJEJjlk0f7DTSRcs8";
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDOES_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
